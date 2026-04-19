ee0b80555bcdbea4b3283655c8d309e659fffc23
subhan513 <subhancs57@gmail.com>
2026-04-19 19:49:09 +0500

    done

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,4 +4,12 @@ import preact from '@preact/preset-vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      }
+    }
+  }
 })
